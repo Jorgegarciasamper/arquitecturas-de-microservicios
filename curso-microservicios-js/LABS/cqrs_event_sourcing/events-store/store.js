9099c91f2d7a78b8c69c9248e75e3fbffe831201
Jorgegarciasamper <43569744+Jorgegarciasamper@users.noreply.github.com>
2026-04-10 11:13:12 +0000

    practicas

---
+++ curso-microservicios-js/LABS/cqrs_event_sourcing/events-store/store.js
@@ -0,0 +1,6 @@
+export const events = [];
+export function save(evento) {
+  events.push(evento);
+  console.log("💾 Guardado en Event Store:", evento.tipo);
+}
+export function all() { return events; }
